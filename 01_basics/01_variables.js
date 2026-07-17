58c2ece4a3357457d63cd2bc6b33705275a96849
pragatidixit <dixitpragati272@gmail.com>
2026-07-17 11:37:07 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=1234;
+let accountEmail="hii@abc";
+var accountPassword="123";
+
+// accountId=4444;
+accountEmail="1234";
+accountPassword="4567";
+
+console.table([accountId,accountEmail,accountPassword]);
